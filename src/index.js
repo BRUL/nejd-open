1b2cada51697b87e56a272345ec2be2e3b9c8963
BRUL <jonathan.dequeker@gmail.com>
2020-05-28 18:16:31 +0200

    removed manual project pages

--- src/index.js
+++ src/index.js
@@ -12,15 +12,7 @@ import './index.scss';
 
 import Header from './components/Header/Header';
 const Projects = lazy(() => import('./containers/Projects/Projects'));
-
-const Gloriette = lazy(() => import('./containers/Project/Gloriette'));
-const FolieADeux = lazy(() => import('./containers/Project/FolieADeux'));
-const Kolom = lazy(() => import('./containers/Project/Kolom'));
-const Still = lazy(() => import('./containers/Project/Still'));
-const Untitled = lazy(() => import('./containers/Project/Untitled'));
-const Reflectie = lazy(() => import('./containers/Project/Reflectie'));
 const Project = lazy(() => import('./containers/Project/Project'));
-
 const Info = lazy(() => import('./containers/Info/Info'));
 const Contact = lazy(() => import('./containers/Contact/Contact'));
 
@@ -36,12 +28,6 @@ render(
             <Switch location={location}>
               <Route exact path='/' component={Projects} />
               <Route exact path='/projects' component={Projects} />
-              <Route exact path='/project/gloriette' component={Gloriette} />
-              <Route exact path='/project/folie-a-deux' component={FolieADeux} />
-              <Route exact path='/project/kolom' component={Kolom} />
-              <Route exact path='/project/still' component={Still} />
-              <Route exact path='/project/untitled' component={Untitled} />
-              <Route exact path='/project/reflectie' component={Reflectie} />
               <Route exact path='/project/:id' component={Project} />
               <Route exact path='/info' component={Info} />
               <Route exact path='/contact' component={Contact} />
